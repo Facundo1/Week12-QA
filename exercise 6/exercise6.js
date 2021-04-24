25fbd4eecea973393d1cd50ed8ce5bd5d5643a87
Facundo Loberse <facundosa123@gmail.com>
2021-04-24 15:58:56 -0300

    exercise 6 modified

--- exercise 6/exercise6.js
+++ exercise 6/exercise6.js
@@ -1,8 +1,11 @@
 //6
 const multiplicationTable = (multiplying, multiplier) => {
-  if (multiplier === undefined) return (multiplier = 10)
-  for (i = 0; i <= multiplier; i++)
-    return multiplying + ' x', i + ' = ', multiplying * i
+  if (multiplier === undefined) multiplier = 10
+  let table = []
+  for (let i = 0; i <= multiplier; i++) {
+    table.push(multiplying * i)
+  }
+  return table
 }
 
 module.exports = multiplicationTable
--- exercise 6/exercise6.test.js
+++ exercise 6/exercise6.test.js
@@ -1,9 +1,21 @@
 const multiplicationTable = require('./exercise6')
 
-test('multiplication Table 2 / 0 is equal to 0', () => {
-  expect(multiplicationTable(undefined)).toBe(10)
+test('if multiplier is undefined then multiplicador is equal to 10', () => {
+  expect(multiplicationTable(5)).toEqual([
+    0,
+    5,
+    10,
+    15,
+    20,
+    25,
+    30,
+    35,
+    40,
+    45,
+    50
+  ])
 })
 
-test('multiplication Table 7 to 7 is equal to 0, 7, 14, 21, 28, 35, 42, 49', () => {
-  expect(multiplicationTable(7, 7)).toBe(0, 7, 14, 21, 28, 35, 42, 49)
+test('multiplicando is 7 and multiplicador 7', () => {
+  expect(multiplicationTable(7, 7)).toEqual([0, 7, 14, 21, 28, 35, 42, 49])
 })
