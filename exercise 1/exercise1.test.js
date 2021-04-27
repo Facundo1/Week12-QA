655dd49cc7dcf818746920d00a6461930c7c016b
Facundo Loberse <facundosa123@gmail.com>
2021-04-27 04:02:09 -0300

    added not.tobe test in exercise 1

--- exercise 1/exercise1.test.js
+++ exercise 1/exercise1.test.js
@@ -4,4 +4,7 @@ describe('Texts tests', () => {
   test('return the text', () => {
     expect(text('text')).toBe('text')
   })
+  test('not return other text different to -text-', () => {
+    expect(text('text')).not.toBe('other-thing')
+  })
 })
